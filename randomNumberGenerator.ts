749fd17bb8b7f56d2dbccae9ed2c9fa7895fda2c
Pontus Tahir <pontus.tahir@gmail.com>
2020-01-17 14:15:04 +0100

    class

---
+++ randomNumberGenerator.ts
@@ -0,0 +1,10 @@
+class randomNumberGenerator {
+    randomNumber: number
+
+    constructor(randomNumber: number) {
+        this.randomNumber = randomNumber
+    }
+
+}
+
+// Math.floor(Math.random() * 100) + 1
