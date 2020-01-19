2ad46bdfff4a8d620e1acfe6dd996ad262004371
Embla <emblajonsson@gmail.com>
2020-01-19 16:40:04 +0100

    popup

--- instructions.ts
+++ instructions.ts
@@ -1,6 +1,15 @@
-class Instructions{
+let button:HTMLButtonElement = document.getElementById("help") as HTMLButtonElement;
+let closebtn:HTMLDivElement = document.getElementsByClassName("close")[0] as HTMLDivElement;
 
-showInstructions(): void{
+button.addEventListener('click', showHelp)
+closebtn.addEventListener('click', hideHelp)
+let modal:HTMLDivElement = document.getElementById("popupDiv") as HTMLDivElement;
 
+
+function showHelp(): void{
+    modal.style.display = "block";
 }
-}
+
+function hideHelp(): void {
+    modal.style.display = "none";
+  }
