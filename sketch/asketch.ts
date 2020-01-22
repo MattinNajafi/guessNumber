cbdb35b93fe736af98294069cd943f294607e75a
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-01-22 08:08:58 +0100

    toggle instruktions

--- sketch/asketch.ts
+++ sketch/asketch.ts
@@ -1,3 +1,33 @@
+window.addEventListener("load", init);
 
-let rng = new randomNumberGenerator()
-let instructions = new Instructions()
+function init(): void {
+  setupEventListenersClick();
+
+  const guessSpan: number = 20; //make the user choose this with a range or dropdown
+
+  // let instructions = new Instructions()
+  let numberGenerator = new NumberGenerator();
+  let rng = numberGenerator.random(guessSpan);
+  console.log(rng);
+}
+
+function setupEventListenersClick() {
+    toggleInstructions();
+    
+}
+
+function toggleInstructions() {
+    let toggleBtn: any = document.querySelector(".toggle-btn");
+    let content: any = document.querySelector(".instructions .content");
+    let open: Boolean = false;
+    
+    toggleBtn.addEventListener("click", function() {
+      if (!open) {
+        content.style.display = "block";
+        open = true;
+      } else if (open) {
+        content.style.display = "none";
+        open = false;
+      }
+    }); 
+}
