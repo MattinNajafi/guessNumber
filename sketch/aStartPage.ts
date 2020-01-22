e2750119cba7f1025b48d63c8106c5848b814a7f
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-01-22 13:27:12 +0100

    added ts-files

--- sketch/aStartPage.ts
+++ sketch/aStartPage.ts
@@ -1,17 +1,15 @@
 // explanation of game phase
-// gamePhase = 0 = display name-input, game has not started
-// gamePhase = 1 = display main menu, game has not started
+// gamePhase = 0 = display start page, game has not started
+// gamePhase = 1 = display game menu, game has not started
 // gamePhase = 2 = display game area, game has started
 // gamePhase = 3 = display game results, game has ended 
 
-// global variables
+window.addEventListener("load", init);
 let gamePhase: number;
 
-window.addEventListener("load", init);
-
-gamePhase = 0;
-
 function init(): void {
+
+  gamePhase = 0;
   setupEventListenersClick();
 
   const guessSpan: number = 20; //make the user choose this with a range or dropdown
@@ -20,7 +18,7 @@ function init(): void {
   let numberGenerator = new NumberGenerator();
   let rng = numberGenerator.random(guessSpan);
   console.log(rng);
-
+ 
 
 }
 
