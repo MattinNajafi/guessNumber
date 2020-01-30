8579ae667a986bf866e59a486a862786e8f96ffe
Embla <emblajonsson@gmail.com>
2020-01-30 09:59:24 +0100

    changed text

--- sketch/CPU.ts
+++ sketch/CPU.ts
@@ -38,7 +38,7 @@ class CPU {
         console.log("Bot wins!");
         (document.querySelector(
           ".conclusion"
-        ) as HTMLHeadingElement).innerText = "Bobot guessed" + ' ' + this.correctAnswer + ' ' + "and won this round!";
+        ) as HTMLHeadingElement).innerText = "Bobot guessed" + ' ' + this.correctAnswer + ' ' + "and won!";
         gamePhase = 3;
         updatePhase(gamePhase);
       } else if (guess < this.correctAnswer) {
@@ -57,7 +57,7 @@ class CPU {
       if (guess == this.correctAnswer) {
         (document.querySelector(
           ".conclusion"
-        ) as HTMLHeadingElement).innerText = "You guessed" + ' ' + this.correctAnswer + ' ' + "and won this round!";
+        ) as HTMLHeadingElement).innerText = "You guessed" + ' ' + this.correctAnswer + ' ' + "and won!";
 
         score = guessList.length;
         gamePhase = 3;
